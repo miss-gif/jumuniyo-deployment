e3563cf9e8f3c17752ef683590eab10d8b28b92e
miss-gif <ddc.akari@gmail.com>
2024-07-09 21:10:30 +0900

    Save : 중간저장

--- src/components/join/Mypage.js
+++ src/components/join/Mypage.js
@@ -28,11 +28,10 @@ const Mypage = () => {
   const currentPath = location.pathname;
 
   const navItems = [
-    { path: "/mypage", label: "개인정보" },
+    { path: "/mypage", label: "계정관리" },
     { path: "/mypage/address", label: "주소관리" },
     { path: "/mypage/order", label: "주문내역" },
     { path: "/mypage/review", label: "리뷰관리" },
-    { path: "/mypage/withdrawal", label: "회원탈퇴" },
   ];
 
   return (
