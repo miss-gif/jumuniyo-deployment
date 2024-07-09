c9095131ef1596177475c5e3dc10bade2943ab75
miss-gif <ddc.akari@gmail.com>
2024-07-09 19:44:10 +0900

    Add : 로그인 페이지

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,7 +1,85 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
-const LoginPage = () => {
-  return <div>LoginPage</div>;
+const Login = () => {
+  return (
+    <div className="login">
+      <div>
+        <h1>
+          <Link to="/">
+            <img
+              src={process.env.PUBLIC_URL + "/images/logo_1x.png"}
+              alt="Logo"
+            />
+          </Link>
+        </h1>
+      </div>
+      <h2 className="login__title">로그인</h2>
+      <form className="login__form">
+        <div className="login__form-group">
+          <label className="login__label" htmlFor="username">
+            아이디
+          </label>
+          <input
+            className="login__input"
+            type="text"
+            id="username"
+            name="username"
+          />
+        </div>
+        <div className="login__form-group">
+          <label className="login__label" htmlFor="password">
+            비밀번호
+          </label>
+          <input
+            className="login__input"
+            type="password"
+            id="password"
+            name="password"
+          />
+        </div>
+        <div className="login__form-group login__form-group--remember">
+          <input
+            className="login__checkbox"
+            type="checkbox"
+            id="remember"
+            name="remember"
+          />
+          <label
+            className="login__label login__label--checkbox"
+            htmlFor="remember"
+          >
+            아이디 기억하기
+          </label>
+        </div>
+        <button className="login__button" type="submit">
+          로그인
+        </button>
+      </form>
+      <div className="login__links">
+        <a className="login__link" href="/signup">
+          회원가입
+        </a>
+        <a className="login__link" href="/forgot-id">
+          아이디 찾기
+        </a>
+        <a className="login__link" href="/forgot-password">
+          비밀번호 찾기
+        </a>
+      </div>
+      <div className="login__social">
+        <button className="login__social-button login__social-button--naver">
+          네이버
+        </button>
+        <button className="login__social-button login__social-button--google">
+          구글
+        </button>
+        <button className="login__social-button login__social-button--kakao">
+          카카오
+        </button>
+      </div>
+    </div>
+  );
 };
 
-export default LoginPage;
+export default Login;
