fc48801f5eee34cbeb94dea325ffee530dc17db1
miss-gif <svx327@naver.com>
2024-07-11 20:43:11 +0900

    Design : 회원가입 선택 페이지

--- src/pages/access/signupPage/SignupChoice.jsx
+++ src/pages/access/signupPage/SignupChoice.jsx
@@ -3,11 +3,13 @@ import React from "react";
 const SignupChoice = () => {
   return (
     <div className="signup-choice">
-      <h1 className="signup-choice__title">회원가입 유형 선택</h1>
-      <p className="signup-choice__description">
-        배달 플랫폼에 가입하여 서비스를 이용하세요. 아래에서 회원가입 유형을
-        선택해 주세요.
-      </p>
+      <div className="signup-choice__text">
+        <h2 className="signup-choice__title">회원가입 유형 선택</h2>
+        <p className="signup-choice__description">
+          배달 플랫폼에 가입하여 서비스를 이용하세요. <br />
+          아래에서 회원가입 유형을 선택해 주세요.
+        </p>
+      </div>
       <div className="signup-choice__options">
         <div className="signup-choice__option">
           <h2 className="signup-choice__option-title">사용자 회원가입</h2>
