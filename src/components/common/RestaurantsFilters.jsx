093a02b8f141d97e4f87e8c513465bfb9de5a9df
miss-gif <svx327@naver.com>
2024-07-12 18:19:26 +0900

    Feat : RestaurantsFilters 연동

--- src/components/common/RestaurantsFilters.jsx
+++ src/components/common/RestaurantsFilters.jsx
@@ -1,10 +1,13 @@
 import React, { useState, useEffect, useRef } from "react";
 import SearchIcon from "@mui/icons-material/Search";
 import ModalMenuSearch from "../../pages/user/restaurantListPage/ModalMenuSearch";
+import useCategories from "../../hooks/useCategories";
 
 const RestaurantsFilters = () => {
   const [isMenuSearchVisible, setMenuSearchVisible] = useState(false);
   const menuSearchRef = useRef(null);
+
+  const { categories, loading, error } = useCategories();
 
   const onClickMenuSearch = () => {
     setMenuSearchVisible(!isMenuSearchVisible);
@@ -26,6 +29,9 @@ const RestaurantsFilters = () => {
     };
   }, []);
 
+  if (loading) return <p>로딩 중...</p>;
+  if (error) return <p>에러 발생: {error}</p>;
+
   return (
     <>
       <div className="background-color">
@@ -36,7 +42,11 @@ const RestaurantsFilters = () => {
             </button>
             <ul className="filters__list">
               <li className="filters__item btn-active">전체보기</li>
-              <li className="filters__item">한식</li>
+              {categories.map(category => (
+                <li key={category.categoryPk} className="filters__item">
+                  {category.categoryName}
+                </li>
+              ))}
               <ModalMenuSearch
                 menuSearchRef={menuSearchRef}
                 isVisible={isMenuSearchVisible}
