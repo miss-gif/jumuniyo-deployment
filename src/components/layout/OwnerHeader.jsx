18fbea1641a3e2c902436841c22504ff3f79721c
miss-gif <ddc.akari@gmail.com>
2024-07-13 05:04:51 +0900

    Design : 사업자 페이지 헤더 작업

--- src/components/layout/OwnerHeader.jsx
+++ src/components/layout/OwnerHeader.jsx
@@ -1,7 +1,24 @@
 import React from "react";
+import Logo from "../common/Logo";
 
 const OwnerHeader = () => {
-  return <div>OwnerHeader</div>;
+  return (
+    <header className="owner-header">
+      <div className="owner-header__left">
+        <h1>
+          <Logo />
+        </h1>
+      </div>
+      <div className="owner-header__right">
+        <div className="owner-header__user">
+          <em>id</em>님
+        </div>
+        <div className="owner-header__item">내정보</div>
+        <div className="owner-header__item">알림</div>
+        <div className="owner-header__item">로그아웃</div>
+      </div>
+    </header>
+  );
 };
 
 export default OwnerHeader;
