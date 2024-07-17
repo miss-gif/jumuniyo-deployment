48e9bc754491628c3a6e267712b16d36f6d183e8
miss-gif <svx327@naver.com>
2024-07-17 20:43:47 +0900

    Test : 카테고리 이동 테스트

--- src/pages/user/homePage/CategoryNavigation.jsx
+++ src/pages/user/homePage/CategoryNavigation.jsx
@@ -1,15 +1,15 @@
-// CategoryNavigation.js
 import React from "react";
 import useCategories from "../../../hooks/useCategories";
 import { useNavigate } from "react-router-dom";
+import { fetchRestaurantList } from "../../../api/fetchRestaurantlist";
 
 const CategoryNavigation = () => {
   const { categories, loading, error } = useCategories();
-
   const navigate = useNavigate();
 
-  const onClickSearch = () => {
-    navigate("/restaurants");
+  const onClickSearch = categoryPk => {
+    fetchRestaurantList(categoryPk); // 카테고리별 식당 리스트를 가져옵니다.
+    navigate("/restaurants"); // 식당 리스트 페이지로 이동합니다.
   };
 
   if (loading) return <p>로딩 중...</p>;
@@ -20,13 +20,20 @@ const CategoryNavigation = () => {
       <h2 className="hidden">카테고리 선택 메뉴</h2>
       <section className="category">
         <ul className="category__list">
-          <li className="category__item border-set bc" onClick={onClickSearch}>
+          <li
+            className="category__item border-set bc"
+            onClick={() => onClickSearch(0)} // 수정된 부분
+          >
             <p>전체보기</p>
           </li>
           {categories.map(category => (
             <li
               key={category.categoryPk}
               className="category__item border-set bc"
+              onClick={() => {
+                console.log(category.categoryPk);
+                onClickSearch(category.categoryPk);
+              }}
             >
               <p>{category.categoryName}</p>
             </li>
