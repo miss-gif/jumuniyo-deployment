f2ba0e05b0b4ff182efbc5fef734e947808285e2
miss-gif <svx327@naver.com>
2024-07-17 20:04:17 +0900

    Test : 카테고리 링크 테스트

--- src/pages/user/homePage/CategoryNavigation.jsx
+++ src/pages/user/homePage/CategoryNavigation.jsx
@@ -1,9 +1,16 @@
 // CategoryNavigation.js
 import React from "react";
 import useCategories from "../../../hooks/useCategories";
+import { useNavigate } from "react-router-dom";
 
 const CategoryNavigation = () => {
   const { categories, loading, error } = useCategories();
+
+  const navigate = useNavigate();
+
+  const a = () => {
+    navigate("/test");
+  };
 
   if (loading) return <p>로딩 중...</p>;
   if (error) return <p>에러 발생: {error}</p>;
@@ -13,7 +20,7 @@ const CategoryNavigation = () => {
       <h2 className="hidden">카테고리 선택 메뉴</h2>
       <section className="category">
         <ul className="category__list">
-          <li className="category__item border-set bc">
+          <li className="category__item border-set bc" onClick={a}>
             <p>전체보기</p>
           </li>
           {categories.map(category => (
